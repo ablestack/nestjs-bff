744a0abd3cc47226d50dc7f6486153b89bbbda05
ahrnee <ahrndee@gmail.com>
2018-07-20 06:33:02 -0500

    Fixed issue with miscongifured provider

--- api/src/cats/cats.module.ts
+++ api/src/cats/cats.module.ts
@@ -3,12 +3,12 @@ import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 import { CommonModule } from '../common/common.module';
 import { AuthModule } from '../auth/auth.module';
-import { catsProvider } from './cats.provider';
+import { catsProviders } from './cats.provider';
 import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [DatabaseModule, CommonModule, AuthModule],
   controllers: [CatsController],
-  providers: [CatsService, catsProvider],
+  providers: [CatsService, ...catsProviders],
 })
 export class CatsModule {}
--- api/src/cats/cats.service.ts
+++ api/src/cats/cats.service.ts
@@ -7,16 +7,16 @@ import { CreateCatDto } from './dto/create-cat.dto';
 export class CatsService {
   constructor(@Inject('CatModelToken') private readonly catModel: Model<Cat>) {}
 
-  async create(createCatDto: CreateCatDto): Promise<Cat> {
+  public async create(createCatDto: CreateCatDto): Promise<Cat> {
     const createdCat = new this.catModel(createCatDto);
     return await createdCat.save();
   }
 
-  async findAll(): Promise<Cat[]> {
+  public async findAll(): Promise<Cat[]> {
     return await this.catModel.find().exec();
   }
 
-  async findOne(id: number): Promise<Cat> {
+  public async findOne(id: number): Promise<Cat> {
     return await this.catModel.findById(id);
   }
 }
