185184fbba333db3e4925b1186c901f9f207e661
ahrnee <ahrndee@gmail.com>
2018-11-17 07:29:25 -0600

    Getting proxy redirect working

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -9,30 +9,37 @@ import { NotificationComponent } from './dashboard/notification/notification.com
 import { SweetAlertComponent } from './dashboard/sweetalert/sweetalert.component';
 import { SettingsComponent } from './dashboard/settings/settings.component';
 import { PriceTableComponent } from './dashboard/component/pricetable/pricetable.component';
-import { PanelsComponent} from './dashboard/component/panels/panels.component';
+import { PanelsComponent } from './dashboard/component/panels/panels.component';
 import { WizardComponent } from './dashboard/component/wizard/wizard.component';
 
 import { RootComponent } from './dashboard/root/root.component';
 import { LoginComponent } from './page/login/login.component';
 import { LockComponent } from './page/lock/lock.component';
 import { RegisterComponent } from './page/register/register.component';
+import { AuthGuard } from './_guards';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'lock', component: LockComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'dashboard', component: RootComponent, children: [
-    {path: '', component: HomeComponent},
-    {path: 'profile', component: ProfileComponent},
-    {path: 'table', component: TableComponent},
-    {path: 'notification', component: NotificationComponent},
-    {path: 'alert', component: SweetAlertComponent},
-    {path: 'settings', component: SettingsComponent},
-    {path: 'components/price-table', component: PriceTableComponent},
-    {path: 'components/panels', component: PanelsComponent},
-    {path: 'components/wizard', component: WizardComponent}
-  ]}
+  { path: 'login', component: LoginComponent },
+  { path: 'lock', component: LockComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'dashboard',
+    component: RootComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'table', component: TableComponent },
+      { path: 'notification', component: NotificationComponent },
+      { path: 'alert', component: SweetAlertComponent },
+      { path: 'settings', component: SettingsComponent },
+      { path: 'components/price-table', component: PriceTableComponent },
+      { path: 'components/panels', component: PanelsComponent },
+      { path: 'components/wizard', component: WizardComponent },
+    ],
+  },
+  // otherwise redirect to dashboard
+  { path: '**', redirectTo: 'dashboard' },
 ];
 
 export const routing = RouterModule.forRoot(routes);
-
