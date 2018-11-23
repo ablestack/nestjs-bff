ebb6fc7bc130ce683e2b7a87aa8cfd65b7c9c181
ahrnee <ahrndee@gmail.com>
2018-11-23 06:11:03 -0600

    Removing unused packages, and adding missing packages

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
-const tsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   entry: ['webpack/hot/poll?1000', './src/main.hmr.ts'],
@@ -24,7 +23,6 @@ module.exports = {
   mode: 'development',
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
-    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
   output: {
