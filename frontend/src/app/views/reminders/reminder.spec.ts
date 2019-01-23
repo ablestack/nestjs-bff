363109d616a3c49203f036b34d67c6670f93a53b
ahrnee <ahrndee@gmail.com>
2019-01-23 06:25:29 -0600

    updated package folder naming convention to support non bff packages

---
+++ frontend/src/app/views/reminders/reminder.spec.ts
@@ -0,0 +1,7 @@
+import { Reminder } from './reminder';
+
+describe('Reminder', () => {
+  it('should create an instance', () => {
+    expect(new Reminder()).toBeTruthy();
+  });
+});
--- packages/pkg-backend/lib-e2e/core/test-object-literals.constants.d.ts
+++
@@ -1,5 +0,0 @@
-import { INestjsBffConfig } from '../../src/config/nestjs-bff.config';
-declare const testData: any;
-export declare const setupTestDataJwtTokens: (nestJsBffConfig: INestjsBffConfig) => Promise<void>;
-export { testData };
-//# sourceMappingURL=test-object-literals.constants.d.ts.map
